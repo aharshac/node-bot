3a196ca88a68284e08d7dcf866283fbeb31c3991
aharshac <aharshac@gmail.com>
2017-02-07 00:32:16 +0530

    Fixed async in motors.js

--- motors.js
+++ motors.js
@@ -14,49 +14,63 @@ motors.pins = {
 
 motors.goForward = function(){
   async.parallel([
-    rpi.pinOut(motors.leftForward, true),
-    rpi.pinOut(motors.rightForward, true);
+    function(callback) {
+			rpi.pinOut(motors.leftForward, true, callback);
+		},
+    function(callback) {
+			rpi.pinOut(motors.rightForward, true, callback);
+		}
   ]);
 };
 
 motors.goReverse = function(){
   async.parallel([
-		rpi.pinOut(motors.leftReverse, true),
-    rpi.pinOut(motors.rightReverse, true);
+		function(callback) {
+			rpi.pinOut(motors.leftReverse, true, callback);
+		}
+    function(callback) {
+			rpi.pinOut(motors.rightReverse, true, callback);
+		}
   ]);
 };
 
 motors.turnLeft = function(){
   async.parallel([
-		rpi.pinOut(motors.leftReverse, true),
-    rpi.pinOut(motors.rightForward, true);
-  ]);
+		function(callback) {
+			rpi.pinOut(motors.leftReverse, true, callback);
+		},
+  	function(callback) {
+			rpi.pinOut(motors.rightForward, true, callback);
+		}
+	]);
 };
 
 motors.turnRight = function(){
   async.parallel([
-		rpi.pinOut(motors.rightReverse, true),
-    rpi.pinOut(motors.leftForward, true);
+		function(callback) {
+			rpi.pinOut(motors.rightReverse, true, callback);
+		},
+    function(callback) {
+			rpi.pinOut(motors.leftForward, true, callback);
+		}
   ]);
 };
 
 motors.stop = function(){
   async.parallel([
-		rpi.pinOut(motors.leftForward, false),
-    rpi.pinOut(motors.rightForward, false),
-		rpi.pinOut(motors.rightReverse, false),
-    rpi.pinOut(motors.leftReverse, false);
+		function(callback) {
+			rpi.pinOut(motors.leftForward, false, callback);
+		},
+		function(callback) {
+    	rpi.pinOut(motors.rightForward, false, callback);
+		},
+		function(callback) {
+			rpi.pinOut(motors.rightReverse, false, callback);
+		},
+    function(callback) {
+			rpi.pinOut(motors.leftReverse, false, callback);
+		}
   ]);
 };
-
-//stop both motors in all directions
-stop: function(){
-  async.parallel([
-    gpio.write(this.motors.leftFront, 0),
-    gpio.write(this.motors.leftBack, 0),
-    gpio.write(this.motors.rightFront, 0),
-    gpio.write(this.motors.rightBack, 0)
-  ]);
-}
 
 module.exports = motors;
