6984698723dd2e165945f002df64886cdd919d9d
aharshac <aharshac@gmail.com>
2017-02-07 09:53:32 +0530

    Fixed namesppace style in rpi.js

--- index.js
+++ index.js
@@ -3,6 +3,7 @@ const motors = require('./motors');
 
 rpi.init(function(error){
   if (!error) {
-    motors.goForward();
+    console.log(typeof rpi.pinHigh);
+    //motors.goForward();
   }
 });
--- rpi.js
+++ rpi.js
@@ -5,89 +5,97 @@ const async = require('async');
 const motors = require('./motors');
 
 
-const rpi = {};
+const rpi = {
+	isRaspberryPi: false,	// Check once if RPi
 
-rpi.isRpi = false;	// Check once if RPi
+	GPIO: null,	// GPIO object
 
-rpi.GPIO = null;	// GPIO object
+	gpio_loaded: false,	// GPIO setup complete?
 
-rpi.gpio_loaded = false;	// GPIO setup complete?
+	os_release_file: "/etc/os-release",	// RPi Raspbian release info
 
-rpi.os_release_file = "/etc/os-release";	// RPi Raspbian release info
+	isRpi: function () {
+		return this.isRaspberryPi;
+	},
 
-// Check if Pi
-rpi.init = function(onInit){
-	if(os.platform().toLowerCase() == 'linux' && fs.existsSync(rpi.os_release_file)){
-		var data = fs.readFileSync(rpi.os_release_file).toString().toLowerCase();
-		rpi.isRpi = (data.indexOf("raspbian") > 0);
-	}
+	getGpio: function () {
+		return this.GPIO;
+	},
 
-	console.log("Raspberry Pi Check : " + rpi.isRpi);
-
-	if (!rpi.isRpi) {
-		if (onInit) {
-			onInit(true);	// error = true;
+	// Check if Pi
+	init: function(onInit) {
+		if(os.platform().toLowerCase() == 'linux' && fs.existsSync(this.os_release_file)){
+			var data = fs.readFileSync(this.os_release_file).toString().toLowerCase();
+			this.isRaspberryPi = (data.indexOf("raspbian") > 0);
 		}
-		return;
+
+		console.log("Raspberry Pi Check : " + this.isRpi());
+
+		if (!this.isRpi()) {
+			if (onInit) {
+				onInit(true);	// error = true;
+			}
+			return;
+		}
+
+		try {
+	    const lib_rpiGpio = require('rpi-gpio');
+	    this.GPIO = lib_rpiGpio;
+		} catch (ex) {
+			console.log("Could not initialize GPIO library.");
+			return;
+		}
+	  this.setupGpio(onInit);
+	},
+
+	// Initialize GPIO
+	setupGpio: function(onSetupComplete) {
+		if(this.isRpi() && this.GPIO != null){
+	    // Repeat for each GPIO pin
+	    async.parallel(
+				[
+		      function(callback) {
+		        this.GPIO.setup(motors.pins.leftForward, this.GPIO.DIR_OUT, callback);    // leftForward
+		      },
+		      function(callback) {
+		        this.GPIO.setup(motors.pins.leftReverse, this.GPIO.DIR_OUT, callback);    // leftReverse
+		      },
+					function(callback) {
+		        this.GPIO.setup(motors.pins.rightForward, this.GPIO.DIR_OUT, callback);    // rightForward
+		      },
+		      function(callback) {
+		        this.GPIO.setup(motors.pins.rightReverse, this.GPIO.DIR_OUT, callback);    // rightReverse
+		      },
+		    ],
+				function(err, results) {
+		      this.gpio_loaded = !err;
+		      if (err) {
+		        console.log("RPi GPIO setup error " + err);
+					}
+					if (onSetupComplete) {
+						onSetupComplete(err);
+					}
+		    }
+			);
+		}
+	},
+
+	pinOut: function(pin, value, callback) {
+	  if(this.isRpi() && this.GPIO != null && this.gpio_loaded) {
+	    this.GPIO.write(pin, value, callback);
+	  } else {
+	    callback(true, null);
+	  }
+		//callback(false, 1);
+	},
+
+	pinHigh: function(pin, callback) {
+		this.pinOut(pin, true, callback);
+	},
+
+	pinLow: function(pin, callback) {
+		this.pinOut(pin, false, callback);
 	}
-
-	try {
-    const lib_rpiGpio = require('rpi-gpio');
-    rpi.GPIO = lib_rpiGpio;
-	} catch (ex) {
-		console.log("Could not initialize GPIO library.");
-		return;
-	}
-  rpi.setupGpio(onInit);
-};
-
-// Initialize GPIO
-rpi.setupGpio = function(onSetupComplete){
-	if(rpi.isRpi && rpi.GPIO != null){
-    // Repeat for each GPIO pin
-    async.parallel(
-			[
-	      function(callback) {
-	        rpi.GPIO.setup(motors.pins.leftForward, rpi.GPIO.DIR_OUT, callback);    // leftForward
-	      },
-	      function(callback) {
-	        rpi.GPIO.setup(motors.pins.leftReverse, rpi.GPIO.DIR_OUT, callback);    // leftReverse
-	      },
-				function(callback) {
-	        rpi.GPIO.setup(motors.pins.rightForward, rpi.GPIO.DIR_OUT, callback);    // rightForward
-	      },
-	      function(callback) {
-	        rpi.GPIO.setup(motors.pins.rightReverse, rpi.GPIO.DIR_OUT, callback);    // rightReverse
-	      },
-	    ],
-			function(err, results) {
-	      rpi.gpio_loaded = !err;
-	      if (err) {
-	        console.log("RPi GPIO setup error " + err);
-				}
-				if (onSetupComplete) {
-					onSetupComplete(err);
-				}
-	    }
-		);
-	}
-};
-
-rpi.pinOut = function(pin, value, callback) {
-  if(rpi.isRpi && rpi.GPIO != null && rpi.gpio_loaded) {
-    rpi.GPIO.write(pin, value, callback);
-  } else {
-    callback(true, null);
-  }
-	//callback(false, 1);
-};
-
-rpi.pinHigh = function(pin, callback) {
-	rpi.pinOut(pin, true, callback);
-};
-
-rpi.pinLow = function(pin, callback) {
-	rpi.pinOut(pin, false, callback);
 };
 
 module.exports = rpi;
