5b9f60f581399004c0a457d68bea2f9c1896f6ed
aharshac <aharshac@gmail.com>
2017-02-07 15:59:18 +0530

    Added server and static content

--- index.js
+++ index.js
@@ -1,9 +1,22 @@
 const rpi = require('./rpi');
-const motors = require('./motors');
+//const motors = require('./motors');
+const server = require('./server');
+
+function initServer() {
+  server.init(function(error, port) {
+    if (error) {
+      console.log(error);
+    } else {
+      console.log('Server started on port ' + port);
+    }
+  });
+}
 
 rpi.init(function(error){
   if (!error) {
     //console.log(typeof rpi.pinHigh);
-    motors.goForward();
+    initServer();
+  } else {
+    console.log(error);
   }
 });
---
+++ server.js
@@ -0,0 +1,64 @@
+const express = require('express');
+const compression = require('compression');
+const serveStatic = require('serve-static');
+//const httpLib = require('http');
+const socketLib = require('socket.io');
+const path = require('path');
+
+const motors = require('./motors');
+
+const server = {
+	app: null,	// Check once if RPi
+
+  port: 5000,
+
+  http: null,
+
+	// Check if Pi
+	init: function(onInit) {
+    server.app = express();
+    server.port = process.env.PORT || 80;
+
+    server.app.set('port', server.port);
+    server.app.use(compression());
+    server.app.use(serveStatic(path.join(__dirname, '/www/index.html'));
+
+    server.io = socketLib(server.app);
+
+    io.sockets.on('connection', server.handleSocketConnection);
+
+    try {
+      //server.http = httpLib.createServer(server.app);
+      server.app.listen(server.port, function() {
+        onInit(false, server.port);
+      });
+    } catch (error) {
+      onInit(error, null);
+    }
+	},
+
+  handleSocketConnection: function(socket) {
+    socket.on('move', function(direction) {
+      switch(direction){
+       case 'up':
+          motors.goForward();
+          break;
+        case 'down':
+          motors.goReverse();
+          break;
+        case 'left':
+          motors.turnLeft();
+          break;
+        case 'right':
+          motors.turnRight();
+          break;
+      }
+    });
+    //listen for stop signal
+    socket.on('stop', function(dir){
+      motors.stop();
+    });
+  }
+};
+
+module.exports = server;
