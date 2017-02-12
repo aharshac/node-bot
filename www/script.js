34d554d1c0a77c73ad6a1485769da7fb1615a8eb
aharshac <aharshac@gmail.com>
2017-02-12 15:05:27 +0530

    Fixed stream path in www/script.js

--- www/script.js
+++ www/script.js
@@ -18,7 +18,7 @@ $( document ).ready(function () {
   };
   var activeClass = 'is-active';
   var isPressed = false;
-  var streamUrl = "http://" + document.location.hostname + "/?action=stream";
+  var streamUrl = "http://" + document.location.hostname + ":9000/?action=stream";
   //var streamUrl = "http://192.168.1.51:9000/?action=stream";
 
   /* Initialise socket.io */
