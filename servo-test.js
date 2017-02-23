5e3d236dffa8abcaedeaebc45708bb93968976b8
Harsha Alva <aharshac@gmail.com>
2017-02-23 20:36:36 +0530

    Modified servo-test.js

--- servo-test.js
+++ servo-test.js
@@ -1,14 +1,24 @@
 const five = require("johnny-five");
 const Raspi = require("raspi-io");
 
-const rpi = require('./rpi');
+//const rpi = require('./rpi');
 
 const board = new five.Board({
   io: new Raspi(),
   repl: false,
 });
 
-var pan = new five.Servo({ pin: rpi.pinNum.cameraPan, center: true });
-var tilt = new five.Servo({ pin: rpi.pinNum.cameraTilt, startAt: 120 });
 
-pan.sweep();
+board.on("ready", function() {
+				try {
+					var pan = new five.Servo({ pin: "P1-12", center: true });
+	var tilt = new five.Servo({ pin: "P1-33", startAt: 120 });
+
+pan.to(90, 500);
+tilt.to(90, 1000);
+				} catch (e) {
+					console.log("RPi GPIO setup error " + e);
+				}
+			});
+
+
